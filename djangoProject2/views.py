ce1e6a0505e551d8676aa28acc45a636761ea914
arjun <wangjun886@126.com>
2021-06-01 22:20:50 +0800

    Initial commit

---
+++ djangoProject2/views.py
@@ -0,0 +1,19 @@
+# -*- codeing = utf-8 -*-
+# @Time : 2021-05-29 23:50
+# @Author : WangJun
+# @File : views.py
+# @Software : PyCharm
+
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def hello(request):
+    return HttpResponse("Hello world!你好")
+
+def runoob(request):
+    #views_name = "菜鸟教程"
+    views_list = ["菜鸟教程1", "菜鸟教程2", "菜鸟教程3"]
+    return render(request, "runoob.html", {"hello": views_list})
+    #context          = {}
+    #context['hello'] = 'Hello World!'
+    #return render(request, 'runoob.html', context)
